2fa11904f0c92fd208b80a696a2cac25111a14b1
Emin <ismemin9@gmail.com>
2021-05-23 23:27:29 +0000

    Done

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
         setTimeout(() => {
             $("#me").html("$" + me);
             $("#me").removeClass("spinner");
-        }, 1500);
+        }, Math.floor(Math.random() * 3000) );
 
 
     });
@@ -47,87 +47,104 @@ $(document).ready(function () {
         }
         if (tempme > tempai) {
             let i = 0;
-            let randomClicks =  Math.floor(Math.random() * 3 ) ;
+            let randomClicks = Math.floor(Math.random() * 3);
+
+
+            let aihtml = $("#ai").html()
+
+            setTimeout(() => {
+                $("#ai").html(" ")
+                $("#ai").addClass("spinner");
+            }, Math.floor(Math.random() * 1000) + 500);
+            setTimeout(() => {
+                $("#ai").html(aihtml)
+                $("#ai").removeClass("spinner")
+            }, Math.floor(Math.random() * 1000) + 2000);
+
             let stopme = setInterval(() => {
                 console.log("randomClicks = " + randomClicks)
                 console.log(i)
-                
+
                 if (i >= randomClicks) {
                     clearInterval(stopme);
                     myif();
-                }else{
+                } else {
 
                     i++;
 
 
 
+                    console.log("tempai = " + tempai)
+                    let num = Math.floor(Math.random() * 99);
+                    console.log("num = " + num)
+                    tempai += num;
 
-                let num = Math.floor(Math.random() * 99);
-            tempai += num;
+                    console.log("tempai + = " + tempai)
+                    if (tempai > 100) {
+                        console.log("ai = " + ai)
+                        ai = tempai
+                        balance += ai;
+                        console.log("ai=temp = " + ai)
+                        $("#bln").html("My balance: $" + balance);
+                        let aihtml = $("#ai").html()
+
+                        setTimeout(() => {
+                            $("#ai").html(" ")
+                            $("#ai").addClass("spinner");
+                        }, Math.floor(Math.random() * 1000) + 500);
+                        setTimeout(() => {
+                            $("#ai").html(aihtml + " +   $" + num)
+                            $("#ai").removeClass("spinner")
+                        }, Math.floor(Math.random() * 1000) + 2000);
+                        clearInterval(stopme);
+                    }
+                    else {
+                        ai = tempai
+
+                        let aihtml = $("#ai").html()
+
+                        setTimeout(() => {
+                            $("#ai").html(" ")
+                            $("#ai").addClass("spinner");
+                        }, Math.floor(Math.random() * 1000) + 500);
+                        setTimeout(() => {
+                            $("#ai").html(aihtml + " +   $" + num)
+                            $("#ai").removeClass("spinner")
+                        }, Math.floor(Math.random() * 1000) + 2000);
 
 
-            if (tempai > 100) {
-                ai = tempai
-                balance += ai;
-                $("#bln").html("My balance: $" + balance);
-                let aihtml = $("#ai").html()
 
-                setTimeout(() => {
-                    $("#ai").html(" ")
-                    $("#ai").addClass("spinner");
-                }, 1000);
-                setTimeout(() => {
-                    $("#ai").html(aihtml + " +   $" + num)
-                    $("#ai").removeClass("spinner")
-                }, 2000);
-            }
-            else {
-                ai = tempai
-
-                let aihtml = $("#ai").html()
-
-                setTimeout(() => {
-                    $("#ai").html(" ")
-                    $("#ai").addClass("spinner");
-                }, 1000);
-                setTimeout(() => {
-                    $("#ai").html(aihtml + " +   $" + num)
-                    $("#ai").removeClass("spinner")
-                }, 2000);
-
-
-
-            }
+                    }
 
                 }
-                
 
 
 
-               
-            }, 3300);
-
-            
 
 
-            
+            }, 5000);
 
 
-        }else{
+
+
+
+
+
+        } else {
             myif();
         }
-       function myif() {
-           console.log("myif")
-        if (tempme < tempai) {
-            balance -= tempme;
-            $("#bln").html("My balance: $" + balance)
+        function myif() {
+            console.log("myif")
+            if (tempme < tempai) {
+                balance -= tempme;
+                $("#bln").html("My balance: $" + balance)
+            }
+            if (tempme > tempai) {
+                balance += ai;
+                $("#bln").html("My balance: $" + balance);
+            }
+
         }
-        if (tempme > tempai) {
-            balance += ai;
-            $("#bln").html("My balance: $" + balance);
-        }
-           
-       }
 
     });
 
@@ -136,14 +153,19 @@ $(document).ready(function () {
     $("#add").click(function (e) {
         e.preventDefault();
         let num = Math.floor(Math.random() * 99);
+        console.log("my num = " + num)
+        console.log("my tempme = " + tempme)
         tempme += num
         if (tempme > 100) {
+            if ($("#ai").hasClass("spinner")) {
+                $("#ai").html("$" + tempai)
+                $("#ai").removeClass("spinner");
+            }
             $("#me").html($("#me").html() + " +   $" + num);
             balance -= tempme;
             $("#bln").html("My balance: $" + balance)
         }
         else {
-            tempme += num
             $("#me").html($("#me").html() + " +   $" + num);
         }
 
